eeb6cf21f924f43304d534be01838210b30e10da
Suraj Bharti <134131358+surajbharti0013@users.noreply.github.com>
2023-07-20 12:27:25 +0000

    js_part01

---
+++ 01_javascriptpart01.js
@@ -0,0 +1,76 @@
+// let pencilPrice = 10;
+// let eraserPrice = 5;
+
+// let totalPrice = pencilPrice + eraserPrice;
+
+// console.log("The total price is : ", pencilPrice + eraserPrice,"Rupees.");
+// console.log(`The total price is :  ${totalPrice}Rupees.`);
+
+// let color = "green";
+
+// if (color === "red") {
+//   console.log("STOP");
+// }
+// if (color === "yellow") {
+//   console.log("Slow down");
+// }
+// if (color === "green") {
+//   console.log("Go");
+// }
+
+// let age = 23;
+// if (age >= 18) {
+//   console.log("you can vote");
+// } else if (age == 23) {
+//   console.log("you should vote");
+// }
+
+// let size = "L"
+
+// if (size === "XL") {
+//     console.log("Price is 250");
+// }
+//  else if (size === "L") {
+//     console.log("Price is 200");
+//   }
+//   else if (size === "M") {
+//     console.log("Price is 100");
+//   }
+//   else {
+//     console.log("Price is 50");
+//   }
+
+/* Nested if else*/
+
+// let marks = 79;
+
+// if (marks >= 33 ){
+//     if (marks >= 80) {
+//       console.log("A+ grade")
+//     }
+//     else if (marks >= 70 || marks <= 79) {
+//       console.log("A grade")
+//     }
+
+//     else {
+//       console.log("Just pass class");
+//     }
+// }
+// else {
+//   console.log("Better luck next time");
+// }
+
+// let  str = 'apple';
+
+// if((str[0] === 'a') && (str.length > 3))
+// {
+//   console.log("Its a good string")
+// }
+// else {
+//   console.log("Its a bad string")
+// }
+
+// let firstName = prompt("Enter your first name");
+// let lastName = prompt("Enter your last name");
+// let fullName = firstName + lastName;
+// alert(`Your first name is ${firstName} and your last name is ${lastName}, your full name is ${firstName} ${lastName} `)
