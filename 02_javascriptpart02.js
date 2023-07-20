3166210a0c7ec5e87f5d5cb95043e3e361a4abad
Suraj Bharti <134131358+surajbharti0013@users.noreply.github.com>
2023-07-20 12:28:21 +0000

    js_part02

---
+++ 02_javascriptpart02.js
@@ -0,0 +1,60 @@
+// let number = 20;
+// if (number % 10 === 0) {
+//   console.log("It is good number")
+// }
+// else {
+//   console.log("It is bad number")
+// }
+
+// let name = prompt("Enter your name");
+// let age = prompt("Enter your age");
+// alert(`Your name is ${name} and your age is ${age}`);
+
+// let quarter = 4;
+// switch(quarter) {
+//     case 1:
+//     console.log("January, February, March");
+//     break;
+//     case 2:
+//     console.log("April, May, June");
+//     break;
+//     case 3:
+//     console.log("July, August, September");
+//     break;
+//     case 4:
+//     console.log("October, November, December");
+//     break; 
+//     default:
+//     console.log("Some error");
+// }
+
+// let text = "Suraj";
+// if (text[0] === "a" || text[0] === "A") {
+//   console.log("Golden String")
+// }
+// else {
+//   console.log("Not Golden String")
+// }
+
+// let num1 = 1;
+// let num2 = 2;
+// let num3 = 3;
+// if ((num1 > num2) && (num1 > num3) ) {
+//   console.log("Number 1 is greater")
+// }
+// else if ((num2 > num1) && (num2 > num3)) {
+//   console.log("Number 2 is greater")
+// }
+// else {
+//   console.log("Number 3 is greater")
+// }
+
+// let num1 = 32;
+// let num2 = 47851;
+
+// if ((num1%2) == (num2%2)) {
+//   console.log("numbers have the same last digit which is",num1%10);
+// }
+// else {
+//   console.log("numbers don't have the same last digit");
+// }
