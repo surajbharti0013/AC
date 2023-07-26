baf72ee213127f63d18753f97e368b5423713a86
Suraj Bharti <134131358+surajbharti0013@users.noreply.github.com>
2023-07-26 14:55:28 +0000

    js_part02

--- 02_javascriptpart02.js
+++ 02_javascriptpart02.js
@@ -33,7 +33,6 @@
 // }
 
 // let size = "L"
-
 // if (size === "XL") {
 //     console.log("Price is 250");
 // }
@@ -46,6 +45,16 @@
 //   else {
 //     console.log("Price is 50");
 //   }
+
+/*else if using Strings*/
+// let button = "signup";
+// if (button == "login") {
+//   console.log("User can purchase");
+// } else if (button == "signup") {
+//   console.log("login or sign up");
+// } else {
+//   console.log("Guest account");
+// }
 
 /* Nested if else*/
 // let marks = 79;
@@ -65,8 +74,8 @@
 //   console.log("Better luck next time");
 // }
 
+/* Practice Qs */
 // let  str = 'apple';
-
 // if((str[0] === 'a') && (str.length > 3))
 // {
 //   console.log("Its a good string")
@@ -75,7 +84,54 @@
 //   console.log("Its a bad string")
 // }
 
+// let num = 17;
+// if((num%3 === 0) && ((num + 1 === 15) || (num - 1 === 11)))
+// {
+//   console.log("safe");
+// }
+// else { console.log("not safe");
+// }
+
+/* Practice Qs Switch statement*/
+// let day = 4;
+// switch(day) {
+//   case 1: console.log("Monday");
+//   break;
+//   case 2: console.log("Tuesday");
+//   break;
+//   case 3: console.log("Wednesday");
+//   break;
+//   case 4: console.log("Thursday");
+//   break;
+//   case 5: console.log("Friday");
+//   break;
+//   case 6: console.log("Saturday");
+//   break;
+//   case 7: console.log("Sunday: fun day");
+//   break;
+//   default: console.log("Wrong day!");
+//   break;
+// }
+
+/* Alert & Prompt */
 // let firstName = prompt("Enter your first name");
 // let lastName = prompt("Enter your last name");
 // let fullName = firstName + lastName;
 // alert(`Your first name is ${firstName} and your last name is ${lastName}, your full name is ${firstName} ${lastName}`)
+
+// let marks = 79;
+// if (marks >= 33 ){
+//     if (marks >= 80) {
+//       console.log("A+ grade")
+//     }
+//     else if (marks >= 70 || marks <= 79) {
+//       console.log("A grade")
+//     }
+
+//     else {
+//       console.log("Just pass class");
+//     }
+// }
+// else {
+//   console.log("Better luck next time");
+// }
