a656ff22d0b277ab6e34cef88fca071b2dc0d390
Suraj Bharti <134131358+surajbharti0013@users.noreply.github.com>
2023-08-15 06:13:24 +0000

    js_part06

--- 06_javascriptpart06.js
+++ 06_javascriptpart06.js
@@ -1,9 +1,9 @@
 /************************ JavaScript part06 ************************/
 
 /************************ Functions in JS ************************/
-function hello() {
-  console.log("Hello");
-}
+// function hello() {
+//   console.log("Hello");
+// }
 // hello();
 
 /************************ if else plus Functions in JS ************************/
@@ -19,31 +19,31 @@ function hello() {
 
 /************************ for loop plus Functions in JS ************************/
 
-function print(){
-  for (let i = 0; i<=5; i++) {
-          console.log(i);
-  }
-}
+// function print(){
+//   for (let i = 0; i<=5; i++) {
+//           console.log(i);
+//   }
+// }
 // print()
 
 /************************** Function that prints a poem **************************/
-function poem2(){
-  console.log("up above world so high");
-  console.log("like a diamond in the sky");
-}
+// function poem2(){
+//   console.log("up above world so high");
+//   console.log("like a diamond in the sky");
+// }
 
-function poem1(){
-  console.log("Twinkle Twinkle, little star");
-  console.log("how i wonder what you are");
-}
+// function poem1(){
+//   console.log("Twinkle Twinkle, little star");
+//   console.log("how i wonder what you are");
+// }
 // poem1()
 // poem2()
 
 /************************** Function that rolls a dice **************************/
-function diceRoll() {
-  let num = (Math.floor(Math.random()*6)+1);
-  console.log(num);
-}
+// function diceRoll() {
+//   let num = (Math.floor(Math.random()*6)+1);
+//   console.log(num);
+// }
 // diceRoll();
 
 /************************** Function with Arguments (numbers)**************************/
@@ -54,27 +54,27 @@ function diceRoll() {
 // sum(51, 20)
 
 /************************** Function with Arguments (string and numbers)**************************/
-function info(name, age) {
-  console.log(`My name is ${name} and age is ${age}`)
-}
+// function info(name, age) {
+//   console.log(`My name is ${name} and age is ${age}`)
+// }
 // info("suraj bharti", 30)
 // info("rohit", 29)
 // info(29) // We cant leave 1st argument blank
 // info("nidhi mittal", ) // We can leave 2nd argument blank
 
 /************************** Create a function that gives us the average of 3 numbers. **************************/
-function calcAvg(a, b, c) {
-  //avg = (a + b + c)/3;
-  console.log((a + b + c)/3)    
-}
+// function calcAvg(a, b, c) {
+//   //avg = (a + b + c)/3;
+//   console.log((a + b + c)/3)    
+// }
 // calcAvg(10, 20, 30)
 
 /************************** Create a function that prints the multiplication table of a number. **************************/
-function printTable(table) {
-  for (let i=table; i<=table*10; i = i+table) {
-          console.log(i);
-  }
-}
+// function printTable(table) {
+//   for (let i=table; i<=table*10; i = i+table) {
+//           console.log(i);
+//   }
+// }
 //printTable(73)
 
 /************************** Return :- return keyword is used to return some value from the function. **************************/
@@ -242,16 +242,16 @@ function printTable(table) {
 // };
 
 /* Methods(Shorthand) */
-const calculator = {
-  num:55,
+// const calculator = {
+//   num:55,
 
-  add(a, b) {
-          return a+b;
-  },
-  sub(a, b) {
-          return a-b;
-  }
-};
+//   add(a, b) {
+//           return a+b;
+//   },
+//   sub(a, b) {
+//           return a-b;
+//   }
+// };
 
 /************************** Assignment(part06) **************************/
 
