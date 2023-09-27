0e4dba6ed4d55677b848885dac5fdf054dc86526
Suraj Bharti <134131358+surajbharti0013@users.noreply.github.com>
2023-09-27 15:23:17 +0000

    js_part01

---
+++ 01_javascriptpart01.js
@@ -0,0 +1,33 @@
+/************************ JavaScript part01 ************************/
+/************************ console ************************/
+// Code written in console is not permenant
+// Console use's REPL (Read evaluate print loop)
+
+/************************ variable ************************/
+// A variable is simply the name of a storage location
+
+// a = 10;
+// b = 5;
+// age = 23;
+// name = "Tony";
+// console.log(a + b);
+
+/************************ DataTypes ************************/
+// a = 25;
+// console.log(typeof a);
+// typeof use to check typeof given variable.
+
+// name = "Tony";
+// console.log(typeof name);
+
+// c = 5.9;
+// console.log(typeof c);
+
+/************************ numbers ************************/
+
+// Positive (14) & negative (-4)
+// Integers(45, -50)
+// Floating numbers - with decimal (4.6, -8.9)
+
+// price = 4.0200;
+// console.log(typeof price);
